9e1be4d06019ffd84ba962a11da44c582b22c2f1
Anton Norman <53868993+Airbag65@users.noreply.github.com>
2021-10-26 15:55:03 +0200

    Create floodfill.py

---
+++ floodfill.py
@@ -0,0 +1,34 @@
+import random
+
+INTX = 10
+INTY = 10
+
+def get_board_size() -> None:
+    global INTX, INTY
+    print("\nAnge x och y koordinater för brädet enligt \"X Y\": ")
+    input_board = str(input(""))
+    print("\nDitt bräde ser ser ut som följande: ")
+    coords = input_board.split(" ")
+    for idx, coord in enumerate(coords):
+        coords[idx] = int(coord)
+    INTY = coords[1]
+    INTX = coords[0] 
+
+
+def gen_board(x: int, y: int):
+    plane = []
+    for i in range(y):
+        plane.append([None] * x)
+        for j in range(x):
+            plane[i][j] = random.randrange(2)
+    return plane
+    
+
+def flood_fill(x: int, y: int) -> None:
+    pass
+
+
+get_board_size()
+board = gen_board(INTX, INTY)
+for array in board:
+    print(array)
